dfe1c233dc982fdbbe9c49d322cf09c6433e309e
Indra211 <indrasena0018@gmail.com>
2023-06-02 11:55:12 +0530

    update

--- array.js
+++ array.js
@@ -2,7 +2,8 @@ let users = [
     {firstname:'indra',lastname:'sena',age:20},
     { firstname: 'Destratum', lastname: 'solutions', age: 19 },
     { firstname: 'taminadu', lastname: 'chennai', age: 18 },
-    { firstname: 'AndhraPrdesh', lastname: 'AMARAVATHI', age: 18 }
+    { firstname: 'AndhraPrdesh', lastname: 'AMARAVATHI', age: 18 },
+    { firstname: 'Siva', lastname: 'redyy', age: 18 }
 ]
 
 
